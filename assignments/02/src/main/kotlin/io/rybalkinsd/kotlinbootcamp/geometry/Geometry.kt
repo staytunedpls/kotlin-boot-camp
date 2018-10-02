d1b82aad2878ad3f843e233de4c14cdb4fb3627a
Sergey Starikov <sergey.starikov@bostongene.com>
2018-10-02 13:45:21 +0300

    All tests for assignment02 passed (no ktlint)

--- assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
+++ assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
@@ -10,9 +10,13 @@ interface Collider {
 /**
  * 2D point with integer coordinates
  */
-class Point(x: Int, y: Int) : Collider {
+data class Point(val x: Int, val y: Int) : Collider {
     override fun isColliding(other: Collider): Boolean {
-        TODO("not implemented")
+        return when (other) {
+            is Point -> this == other
+            is Bar -> other.isColliding(this)
+            else -> throw Exception("What was provided as an argument to Point?")
+        }
     }
 }
 
@@ -22,8 +26,24 @@ class Point(x: Int, y: Int) : Collider {
  * Bar is not oriented
  * (It does not matter, which opposite corners you choose to define bar)
  */
-class Bar(firstCornerX: Int, firstCornerY: Int, secondCornerX: Int, secondCornerY: Int) : Collider {
+data class Bar(val firstCornerX: Int,
+               val firstCornerY: Int,
+               val secondCornerX: Int,
+               val secondCornerY: Int) : Collider {
     override fun isColliding(other: Collider): Boolean {
-        TODO("not implemented")
+        when (other) {
+            is Point -> {
+                val xInside: Boolean = (this.firstCornerX <= other.x) && (other.x <= this.secondCornerX)
+                val yInside: Boolean = (this.firstCornerY <= other.y) && (other.y <= this.secondCornerY)
+                return xInside && yInside
+            }
+            is Bar -> {
+                for (x in listOf(this.firstCornerX, this.secondCornerX))
+                    for (y in listOf(this.firstCornerY, this.secondCornerY))
+                        if (other.isColliding(Point(x, y))) return true
+                return false
+            }
+            else -> throw Exception("What argument did you provide to Bar.isColliding?")
+        }
     }
 }
