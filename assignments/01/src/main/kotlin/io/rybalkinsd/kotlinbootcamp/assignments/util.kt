63aed3e8804db6611d72d2dc9f3334dc0fd0208e
Sergey Starikov <sergey.starikov@bostongene.com>
2018-10-02 10:29:14 +0300

    It seems that Intellij commenting on ctrl-/ cannot produce spaces necessary for ktlint

--- assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
+++ assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
@@ -6,18 +6,18 @@ package io.rybalkinsd.kotlinbootcamp.assignments
  * @param values an argument. !! Assume values.length > 0. !!
  * @return the largest of values.
  */
-//fun max(values: List<Int>): Int {
+// fun max(values: list<int>): int {
 //    var max= values[0]
 //    for (value in values)
 //        max = if (value > max) value else max
 //    return max
-//}
+// }
 
-//fun max(values: List<Int>): Int {
+// fun max(values: List<Int>): Int {
 //    var max : Int = values[0]
 //    values.forEach { max = if (it > max) it else max }
 //    return max
-//}
+// }
 
 fun max(values: List<Int>): Int? = values.max()
 
@@ -25,9 +25,9 @@ fun max(values: List<Int>): Int? = values.max()
 // So .toLong is required
 
 // This version falls on "medium size list test" - I guess because of large int values
-//fun sum(values: List<Int>): Long = values.sum().toLong()
+// fun sum(values: List<Int>): Long = values.sum().toLong()
 
-//This one is better:
+// This one is better:
 
 /**
  * Returns the sum of all `int` values.
