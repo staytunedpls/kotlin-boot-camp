5a281c02b9dc062983328103372cb4f81962e4db
Sergey Starikov <sergey.starikov@bostongene.com>
2018-10-02 10:09:58 +0300

    This new util.kt seems to pass all tests

--- assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
+++ assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
@@ -6,7 +6,28 @@ package io.rybalkinsd.kotlinbootcamp.assignments
  * @param values an argument. !! Assume values.length > 0. !!
  * @return the largest of values.
  */
-fun max(values: List<Int>): Int = TODO()
+//fun max(values: List<Int>): Int {
+//    var max= values[0]
+//    for (value in values)
+//        max = if (value > max) value else max
+//    return max
+//}
+
+//fun max(values: List<Int>): Int {
+//    var max : Int = values[0]
+//    values.forEach { max = if (it > max) it else max }
+//    return max
+//}
+
+fun max(values: List<Int>): Int? = values.max()
+
+// Long is needed in case of large numbers, sum returns same value type as list (Int)
+// So .toLong is required
+
+// This version falls on "medium size list test" - I guess because of large int values
+//fun sum(values: List<Int>): Long = values.sum().toLong()
+
+//This one is better:
 
 /**
  * Returns the sum of all `int` values.
@@ -14,4 +35,10 @@ fun max(values: List<Int>): Int = TODO()
  * @param values an argument. Assume values.length > 0.
  * @return the sum of all values.
  */
-fun sum(values: List<Int>): Long = TODO()
+
+fun sum(values: List<Int>): Long {
+    var fullSum : Long = 0
+    values.forEach { fullSum += it }
+    return fullSum
+}
+
